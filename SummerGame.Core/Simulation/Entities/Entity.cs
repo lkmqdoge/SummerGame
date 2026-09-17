b0ae47a23197eec873b4e0c90e677e7fa4e839f9
lkmqdoge <qdoge2335@gmail.com>
2026-09-17 14:33:13 +0300

    fixed chunk generation

---
+++ SummerGame.Core/Simulation/Entities/Entity.cs
@@ -0,0 +1,10 @@
+namespace SummerGame.Core.Simulation.Entities;
+
+public abstract class Entity
+{
+    //TODO:
+    // GlobalPosition
+    // Chunk Bound Checking 
+}
+
+
--- SummerGame.Core/Simulation/Player.cs
+++ SummerGame.Core/Simulation/Player.cs
@@ -1,14 +1,18 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using SummerGame.Core.Graphics;
+using SummerGame.Core.Input;
 
 namespace SummerGame.Core.Simulation;
 
 public class Player(GameCore game) : GameObject(game)
 {
     public Vector2 GlobalPosition { get; set; }
+    public float Speed { get; set; } = 10;
 
     public Sprite2D Sprite = new ();
+
+    private ActionManager _actionManager = game.Simulation.ActionManager;
 
     public override void LoadContent()
     {
@@ -17,6 +21,8 @@ public class Player(GameCore game) : GameObject(game)
 
     public override void Update(double delta)
     {
+        var dir = _actionManager.GetVector("move_left", "move_right", "move_up", "move_down");
+        GlobalPosition += dir * Speed;
     }
 }
 
